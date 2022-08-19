af7af4ea6d482b6c8736f18781055abbb0784f7f
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-08-19 18:15:09 +0600

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,30 @@
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// -----
+// -----
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    int count = 1;
+    while ( i <= n )
+    {
+        int j = 1;
+        while (j <= 3)
+        {
+            cout << count << " ";
+            count++;
+            j++;
+        }
+        i++;
+        cout << endl;
+    }
+}
